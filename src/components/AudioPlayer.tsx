2c7da7869580bc29a388cbde2954bc912d16b4c4
Rich1997 <rudronil1997@gmail.com>
2024-09-08 19:21:20 +0530

    Add now playing drawer, UI tweaks

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import { FaCirclePause, FaCirclePlay } from "react-icons/fa6";
 import { HiVolumeOff, HiVolumeUp } from "react-icons/hi";
+import { IoMdClose } from "react-icons/io";
 import Thumbnail from "./ui snippets/Thumbnail";
 
 type AudioPlayerProps = {
@@ -25,6 +26,7 @@ const AudioPlayer: React.FC<AudioPlayerProps> = ({
     const audioRef = useRef<HTMLAudioElement>(null);
     const [volume, setVolume] = useState(1);
     const [isMuted, setIsMuted] = useState(false);
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
     useEffect(() => {
         if (audioRef.current) {
@@ -44,6 +46,18 @@ const AudioPlayer: React.FC<AudioPlayerProps> = ({
         }
     }, [audioUrl]);
 
+    useEffect(() => {
+        if (isDrawerOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, [isDrawerOpen]);
+
     const handleVolumeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newVolume = parseFloat(event.target.value);
         setVolume(newVolume);
@@ -61,12 +75,25 @@ const AudioPlayer: React.FC<AudioPlayerProps> = ({
         }
     };
 
+    const toggleDrawer = (e: React.MouseEvent) => {
+        if ((e.target as HTMLElement).closest(".controls-area")) {
+            return;
+        }
+        setIsDrawerOpen(!isDrawerOpen);
+    };
+
     return (
-        <div className="h-full w-full bg-card">
+        <div className="relative h-full w-full bg-card">
             <audio ref={audioRef} />
-            <div className="flex items-center justify-between h-full gap-4">
+            <div className="flex items-center justify-between h-full gap-4 p-4 cursor-pointer" onClick={toggleDrawer}>
                 <div className="flex gap-4">
-                    <button onClick={onPlayPause}>
+                    <button
+                        className="controls-area"
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            onPlayPause();
+                        }}
+                    >
                         {isPlaying ? (
                             <FaCirclePause size={36} className="text-primary" />
                         ) : (
@@ -85,8 +112,13 @@ const AudioPlayer: React.FC<AudioPlayerProps> = ({
                         </div>
                     </div>
                 </div>
-                <div className="sm:flex hidden items-center gap-2">
-                    <button onClick={toggleMute}>
+                <div className="sm:flex hidden items-center gap-2 controls-area">
+                    <button
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            toggleMute();
+                        }}
+                    >
                         {isMuted ? <HiVolumeOff size={24} /> : <HiVolumeUp size={24} />}
                     </button>
 
@@ -98,8 +130,62 @@ const AudioPlayer: React.FC<AudioPlayerProps> = ({
                         step="0.01"
                         value={isMuted ? 0 : volume}
                         onChange={handleVolumeChange}
+                        onClick={(e) => e.stopPropagation()}
                         style={{ "--value": `${(isMuted ? 0 : volume) * 100 - 1}%` } as React.CSSProperties}
                     />
+                </div>
+            </div>
+
+            {/* Drawer */}
+            <div
+                className={`fixed bottom-0 left-0 w-full h-full bg-card z-10 transition-transform transform ${
+                    isDrawerOpen ? "translate-y-0" : "translate-y-full"
+                }`}
+                style={{ height: "100%" }}
+            >
+                <div className="container mx-auto p-4 h-full">
+                    <div className="flex justify-between items-center mb-6">
+                        <h2 className="text-2xl font-bold">Now Playing</h2>
+                        <button onClick={() => setIsDrawerOpen(false)} className="text-2xl">
+                            <IoMdClose />
+                        </button>
+                    </div>
+                    <div className="flex flex-col items-center gap-4 justify-center h-full pb-[128px]">
+                        <div className="flex gap-4">
+                            <Thumbnail size="200" imgSrc={thumb} />
+                            <div>
+                                <h3 className="text-xl font-semibold">{stationName}</h3>
+                                <p className="text-muted-foreground">
+                                    {stationCountry}
+                                    {stationCountry && stationState ? ", " : ""}
+                                    {stationState ?? stationState}
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col items-center w-1/2 gap-4">
+                            <div className="flex gap-4 mt-4">
+                                <button onClick={onPlayPause} className="text-4xl">
+                                    {isPlaying ? <FaCirclePause /> : <FaCirclePlay />}
+                                </button>
+                            </div>
+                            <input
+                                className="slider"
+                                type="range"
+                                min="0"
+                                max="1"
+                                step="0.01"
+                                value={isMuted ? 0 : volume}
+                                onChange={handleVolumeChange}
+                                onClick={(e) => e.stopPropagation()}
+                                style={
+                                    {
+                                        "--value": `${(isMuted ? 0 : volume) * 100}%`,
+                                        width: "100%",
+                                    } as React.CSSProperties
+                                }
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/ui snippets/Titlebar.tsx
+++ src/components/ui snippets/Titlebar.tsx
@@ -6,7 +6,7 @@ interface TitlebarProps {
 
 const Titlebar: React.FC<TitlebarProps> = ({ children }) => {
     return (
-        <div className="px-4 text-2xl font-bold tracking-tight sm:pb-6 pb-4 sm:leading-snug leading-5 sm:pt-0 pt-2">
+        <div className="px-4 text-2xl font-bold tracking-tight pb-6 sm:leading-snug leading-5 sm:pt-0 pt-2">
             {children}
         </div>
     );
