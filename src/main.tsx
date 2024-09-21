b94169265ca01279af09acd55ae01dc90c978f43
Rich1997 <rudronil1997@gmail.com>
2024-09-21 16:04:57 +0530

    Add separate css for themes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "./themes.css";
 import { RadioProvider } from "./context/RadioContext.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
 
