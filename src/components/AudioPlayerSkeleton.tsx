03df9e39983341b150489c40693f955e1e9db01c
Rich1997 <rudronil1997@gmail.com>
2024-09-23 02:18:56 +0530

    Minor UI change

--- src/components/AudioPlayerSkeleton.tsx
+++ src/components/AudioPlayerSkeleton.tsx
@@ -1,18 +1,12 @@
-import { FaPlay } from "react-icons/fa6";
-
 const AudioPlayerSkeleton = () => {
     return (
-        <div className="flex w-full h-full select-none items-center p-4 gap-4">
+        <div className="flex w-full h-full select-none items-center p-4 sm:gap-4 gap-0">
             <div className="flex items-center justify-center">
-                <div className="rounded-full sm:h-10 sm:w-10 h-12 w-12 bg-background/70 flex items-center justify-center text-muted">
-                    <div className="pl-0.5">
-                        <FaPlay size={18} />
-                    </div>
-                </div>
+                <div className="rounded-full h-12 w-12 bg-background/70 items-center justify-center text-muted sm:block hidden" />
             </div>
-            <div className="flex items-center sm:gap-4 gap-2">
-                <div className="sm:h-[72px] h-16 sm:w-[72px] w-16 min-h-16 min-w-16 bg-background/70 rounded-lg" />
-                <div className="text-muted-foreground">No station currently playing</div>
+            <div className="flex items-center gap-4 w-full">
+                <div className="h-[72px] w-[72px] bg-background/70 rounded-lg sm:block hidden" />
+                <div className="text-muted-foreground sm:mx-0 mx-auto">No station currently playing</div>
             </div>
         </div>
     );
