10a06f32a139d5c1f09b72f473a442d7c9bba6ad
Rich1997 <rudronil1997@gmail.com>
2024-09-24 01:01:02 +0530

    Change animated wave color

--- src/components/ui snippets/AnimatedWave.tsx
+++ src/components/ui snippets/AnimatedWave.tsx
@@ -2,14 +2,14 @@ const AnimatedWave = () => {
     return (
         <div className="flex h-full items-center justify-center px-2">
             <div
-                className="animate-wave mx-0.5 h-2 w-1 rounded bg-foreground/70"
+                className="animate-wave mx-0.5 h-2 w-1 rounded bg-muted-foreground"
                 style={{ animationDelay: "-0.4s" }}
             ></div>
             <div
-                className="animate-wave mx-0.5 h-3 w-1 rounded bg-foreground/70"
+                className="animate-wave mx-0.5 h-3 w-1 rounded bg-muted-foreground"
                 style={{ animationDelay: "-0.2s" }}
             ></div>
-            <div className="animate-wave mx-0.5 h-4 w-1 rounded bg-foreground/70"></div>
+            <div className="animate-wave mx-0.5 h-4 w-1 rounded bg-muted-foreground"></div>
         </div>
     );
 };
