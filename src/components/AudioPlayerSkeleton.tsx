6a45e20ba15eff2d8fa553c1eda66ab1b7798333
Rich1997 <rudronil1997@gmail.com>
2024-09-24 00:24:28 +0530

    Change theme colors

--- src/components/AudioPlayerSkeleton.tsx
+++ src/components/AudioPlayerSkeleton.tsx
@@ -2,10 +2,10 @@ const AudioPlayerSkeleton = () => {
     return (
         <div className="flex w-full h-full select-none items-center p-4 sm:gap-4 gap-0">
             <div className="flex items-center justify-center">
-                <div className="rounded-full h-10 w-10 bg-background/70 items-center justify-center text-muted sm:block hidden" />
+                <div className="rounded-full h-10 w-10 bg-border items-center justify-center text-muted sm:block hidden" />
             </div>
             <div className="flex items-center gap-4 w-full">
-                <div className="h-[72px] w-[72px] bg-background/70 rounded-lg sm:block hidden" />
+                <div className="h-[72px] w-[72px] bg-border rounded-lg sm:block hidden" />
                 <div className="text-muted-foreground sm:mx-0 mx-auto">No station currently playing</div>
             </div>
         </div>
