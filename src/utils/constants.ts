8d6d7be2abc22c6f47a949767c6fd45a8fb01dd2
Rich1997 <rudronil1997@gmail.com>
2024-10-30 03:52:08 +0530

    Fix type issue

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,14 +1,10 @@
-export const colors = [
-    {
-        colorName: "default",
-        colorAlias: "Default",
-    },
-    {
-        colorName: "blue",
-        colorAlias: "Blue",
-    },
-    {
-        colorName: "gold",
-        colorAlias: "Gold",
-    },
-] as const;
+interface ColorOption {
+    colorName: string;
+    colorAlias: string;
+}
+
+export const colors: ColorOption[] = [
+    { colorName: "default", colorAlias: "Default" },
+    { colorName: "blue", colorAlias: "Blue" },
+    { colorName: "gold", colorAlias: "Gold" },
+];
